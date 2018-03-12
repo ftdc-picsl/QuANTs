4bef1e7f490eecfefcd20762132e2151ca0c6f26
Jeffrey Duda <jeff.duda@gmail.com>
2018-03-12 17:23:23 -0400

    ADD: labels as package data

---
+++ R/mindBoggleLabels.R
@@ -0,0 +1,16 @@
+#' mindBoggleLabels
+#'
+#' A dataset containing mind boggle labels and associated information:
+#'
+#' @format A data frame with 201 rows and 8 variables:
+#' \itemize{
+#'   \item Number: number used in labeling of image
+#'   \item Common: does this label typically appear in the data
+#'   \item Name: full name of region
+#'   \item Abbreviation: abbreviated name of region
+#'   \item Hemisphere: right or left
+#'   \item Lobe: lobe groupings
+#'   \item Cortex: is this in cortex
+#'   \item Bilateral: label grouping together bilateral regions
+#' }
+"mindBoggleLabels"
