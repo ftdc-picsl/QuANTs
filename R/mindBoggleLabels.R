cb417e4239c352f594388469c6e6f27c44b88b3f
Jeffrey Duda <jeff.duda@gmail.com>
2018-03-13 13:33:56 -0400

    ENH: lowercase column names

--- R/mindBoggleLabels.R
+++ R/mindBoggleLabels.R
@@ -4,14 +4,14 @@
 #'
 #' @format A data frame with 201 rows and 8 variables:
 #' \itemize{
-#'   \item Number: number used in labeling of image
-#'   \item Common: does this label typically appear in the data
-#'   \item Name: full name of region
-#'   \item Abbreviation: abbreviated name of region
-#'   \item Hemisphere: right or left
-#'   \item Lobe: lobe groupings
-#'   \item Cortex: is this in cortex
-#'   \item Bilateral: label grouping together bilateral regions
+#'   \item number: number used in labeling of image
+#'   \item common: does this label typically appear in the data
+#'   \item name: full name of region
+#'   \item abbreviation: abbreviated name of region
+#'   \item hemisphere: right or left
+#'   \item lobe: lobe groupings
+#'   \item cortex: is this in cortex
+#'   \item bilateral: label grouping together bilateral regions
 #'  }
 #' @examples
 #' data(mindBoggleLabels)
