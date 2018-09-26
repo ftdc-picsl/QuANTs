f4d0550c6a93cd0d8f47dae1063fcc2e6ee8386d
Jeff Duda <jeff.duda@gmail.com>
2018-09-26 14:42:38 -0400

    FIX: qc file creation

--- R/quants_qc.R
+++ R/quants_qc.R
@@ -6,6 +6,33 @@ quants_qc <- function(subjects="", qc_file="") {
   app = paste(sep="", .libPaths()[1], "/QuANTs/shiny/quants_qc/quants_qc_app.R")
   options(browser="/usr/bin/firefox")
 
+  if ( subjects != "" ) {
+    if ( !file.exists( subjects) ) {
+      stop(paste0("The subject file '", subjects, "' does not exist"))
+    }
+  }
+
+  if ( qc_file != "" ) {
+    if ( !file.exists( qc_file ) ) {
+      df <- data.frame(Date=as.Date(character()),
+                 File=character(),
+                 User=character(),
+                 stringsAsFactors=FALSE)
+
+      df = data.frame( INDDID=character(),
+        Timepoint=character(),
+        Reviewer=character(),
+        T1Quality=integer(),
+        ExtractQuality=integer(),
+        SegmentationQuality=integer(),
+        Movement=integer(),
+        Artefact=integer(),
+        Timestamp=as.Date(character()),
+        Notes=character() )
+      write.csv(df, qc_file, row.names=F)
+    }
+  }
+
   .GlobalEnv$.quants_info = list(subjects=subjects, qc_file=qc_file)
   on.exit(rm(.quants_info, envir=.GlobalEnv))
 
