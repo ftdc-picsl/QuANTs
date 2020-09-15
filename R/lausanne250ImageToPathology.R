3522c0630b98723910758b3d7f0e2140243520b0
Jeffrey Duda <jeff.duda@gmail.com>
2020-09-15 17:42:42 -0400

    FIX: bug fixed

--- R/lausanne250ImageToPathology.R
+++ R/lausanne250ImageToPathology.R
@@ -3,10 +3,7 @@ lausanne250ImageToPathology = function( img ) {
   oImg = img*0
   for ( l in 1:length(lausanne250Labels$number) ) {
     if ( lausanne250Labels$PathologyLabel[l] > 0 ) {
-      idx = which( img==lausanne250Labels$number[l] )
-      if ( length(idx) > 0 ) {
-        oImg[idx] = lausanne250Labels$PathologyLabel[l]
-      }
+      oImg[img==lausanne250Labels$number[l]] = lausanne250Labels$PathologyLabel[l]
     }
   }
 
