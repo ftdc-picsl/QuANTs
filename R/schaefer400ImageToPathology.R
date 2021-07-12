e8c316b8cd4c73c30accfeb4c6ea1567384a92dc
Jeffrey Duda <jeff.duda@gmail.com>
2021-07-12 18:22:36 -0400

    FIX: correct labels

--- R/schaefer400ImageToPathology.R
+++ R/schaefer400ImageToPathology.R
@@ -1,9 +1,9 @@
 schaefer400ImageToPathology = function( img ) {
 
   oImg = img*0
-  for ( l in 1:length(lausanne250Labels$number) ) {
-    if ( lausanne250Labels$PathologyLabel[l] > 0 ) {
-      oImg[img==lausanne250Labels$number[l]] = lausanne250Labels$PathologyLabel[l]
+  for ( l in 1:length(schaefer400Labels$number) ) {
+    if ( schaefer400$PathologyLabel[l] > 0 ) {
+      oImg[img==schaefer400Labels$number[l]] = schaefer400Labels$PathologyLabel[l]
     }
   }
 
