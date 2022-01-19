1a92e644b1108464d32ea409b0869acad0433ede
Jeffrey Duda <jeff.duda@gmail.com>
2022-01-19 14:45:15 -0500

    ADD: script for dataset

---
+++ python/runFTDC.py
@@ -0,0 +1,46 @@
+import itk
+import quantsifier as qf
+import quantsUtilities as qu
+import numpy as np
+import pandas as pd
+import os 
+import sys
+
+
+def parsePath( path ):
+
+    dirParts = os.path.split(dir.rstrip('/'))
+    sesTag = dirParts[1]
+    subTag = os.path.split(dirParts[0])[1]
+
+    id = subTag.split('-')[1]
+    ses = sesTag.split('-')[1]
+
+    return( (id,ses))
+
+#path = "/Users/jtduda/quants-fw-test/antsct"
+path = sys.argv[1]
+odir = sys.argv[2]
+
+
+subDirs = os.listdir(path)
+for sub in subDirs:
+    sesDirs = os.listdir( os.path.join(path, sub) ) 
+    for ses in sesDirs:
+        dir = os.path.join(path, sub, ses)
+        print(dir)
+
+
+        bidsInfo = parsePath(dir)
+        filenames =  qu.getFTDCInputs(dir)
+
+        x = qu.getFTDCQuantsifier(filenames)
+        x.SetConstants({"id": bidsInfo[0], "date": bidsInfo[1]})
+        x.Update()
+        stats = x.GetOutput()
+
+        pd.set_option("display.max_rows", None, "display.max_columns", None)
+        ofile = os.path.join(odir, bidsInfo[0]+"_"+bidsInfo[1]+"_quants.csv")
+        stats.to_csv(ofile, index=False, float_format='%.4f')
+
+print("Done")
