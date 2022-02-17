1f58183e565500d633ee97a691184b0ae4737278
Jeffrey Duda <jeff.duda@gmail.com>
2022-02-17 13:41:19 -0500

    ENH: python package

---
+++ python/quants/quants/__init__.py
@@ -0,0 +1,8 @@
+from .quantsifier import Quantsifier
+from .quantsUtilities import getFTDCInputs
+
+def whatIsQuants():
+    print("quants = quantifying ants")
+    
+def testQuants():
+    print("testQuants()")
---
+++ python/quants/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='quants',
+      version='0.1',
+      description='Summary measures for ANTsCT pipelines',
+      url='https://github.com/ftdc-picsl/quants',
+      author='Jeffrey Duda',
+      author_email='jeff.duda@gmail.com',
+      license='MIT',
+      packages=['quants'],
+      zip_safe=False)
