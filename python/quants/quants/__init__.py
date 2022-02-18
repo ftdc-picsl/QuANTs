23299a16eceb0e057e8a40f7cd2ede3f7ebc2222
Jeffrey Duda <jeff.duda@gmail.com>
2022-02-17 19:27:09 -0500

    FIX: update imports

--- python/quants/quants/__init__.py
+++ python/quants/quants/__init__.py
@@ -1,6 +1,6 @@
 from .quantsifier import Quantsifier
 from .quantsifier import getFTDCQuantsifier
-from .quantsUtilities import getFTDCInputs
+from .quantsifier import getFTDCInputs
 
 def whatIsQuants():
     print("quants = quantifying ants")
