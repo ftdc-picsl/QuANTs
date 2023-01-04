8b16aee4fa1c0d273d23221dcab28197b05d6e01
Jeffrey Duda <jeff.duda@gmail.com>
2023-01-03 22:01:14 -0500

    FIX: imports

--- python/quants/quants/__init__.py
+++ python/quants/quants/__init__.py
@@ -3,7 +3,7 @@ from .quantsifier import getFTDCQuantsifier
 from .quantsifier import getFTDCInputs
 from .quantsifier import getNetworks
 from .image_itk_ants import ants_2_sitk
-from .image_itk_ants import sitk_2_image
+from .image_itk_ants import sitk_2_ants
 
 def whatIsQuants():
     print("quants = quantifying ants")
