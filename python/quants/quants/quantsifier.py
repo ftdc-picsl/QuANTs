FIX: masking issue

python/quants/quants/quantsifier.py
@@ -297,20 +297,15 @@ class Quantsifier():
             if 'Masking' in r:
 
                 tissues = [ self.tissueNames[x] for x in r['Masking']['Include'] ]
-                print(r) 
-                print(tissues)
 
                 mask = self.GetSegmentationMask(tissues)
                 rImg = self.GetSingleLabel(origLabels, int(lbl)) 
-                rImgSum = np.sum(sitk.GetArrayViewFromImage(rImg))/float(lbl)
+                #rImgSum = np.sum(sitk.GetArrayViewFromImage(rImg))/float(lbl)
 
                 maskedLabel = sitk.Multiply(rImg, mask)
-                maskedSum = np.sum(sitk.GetArrayViewFromImage(maskedLabel))/float(lbl)
+                #maskedSum = np.sum(sitk.GetArrayViewFromImage(maskedLabel))/float(lbl)
 
                 maskedLabels = sitk.Add(maskedLabels, maskedLabel)
-
-                print(rImgSum)
-                print(maskedSum)
 
         if networkName in self.label_propagation:
             self.log.info("Apply label propagation for "+networkName+"")
@@ -395,7 +390,6 @@ class Quantsifier():
                     resample.SetInterpolator( sitk.sitkLabelGaussian )
                     resample.SetNumberOfThreads(1)
 
-                    self.log.info("Warping atlas labels into subject space")
                     subLabels = resample.Execute(nImg)
                     maskedLabels = self.ApplyNetworkMasking(network, subLabels)
                     if self.saveImages: